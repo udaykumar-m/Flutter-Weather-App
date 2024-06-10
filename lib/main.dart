d51587c693aab5e24c6ccdeb9c16a16ae6587368
udaykumar-m <udaykumardevil3830@gmail.com>
2024-06-10 13:10:27 +0530

    cleanup

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 
 import 'package:flutter/material.dart';
-import 'package:openai_app/APICall/APIcall.dart';
-import 'package:openai_app/model/api_model.dart';
 
 void main() {
   runApp(const MainApp());
@@ -14,54 +12,9 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       home: Scaffold(
-        body: APIData(),
+        body: ,
       ),
     );
   }
 }
 
-
-class APIData extends StatefulWidget {
-  const APIData({super.key});
-
-  @override
-  State<APIData> createState() => _APIDataState();
-}
-
-class _APIDataState extends State<APIData> {
-
-  final List<Welcome>? userModel = [];
-
-  @override
-  void initState() {
-    super.initState();
-    _getData();
-  }
-
-  dynamic _getData() async{
-    final user = await ApiService().getUsers();
-    user?.forEach((x) => userModel?.add(x) );
-    setState(() {
-      
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return 
-    (userModel?.isEmpty ?? true )? Center(child: CircularProgressIndicator()) : ListView.builder(
-      shrinkWrap: true,
-      itemCount: userModel?.length,
-      itemBuilder: (context, index){
-        return ListTile(
-          title: Column(
-            children: [
-              Text("Id : $index"),
-              Text(userModel![index].body.toString()),
-            ],
-          ),
-        );
-      }
-      );
-  }
-}
