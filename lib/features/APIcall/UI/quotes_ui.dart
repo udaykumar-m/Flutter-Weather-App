016af6546bccc12cd8edde97b04cd0cdb74fe5f1
udaykumar-m <udaykumardevil3830@gmail.com>
2024-06-24 18:02:42 +0530

    added tap to copy for quotes

--- lib/features/APIcall/UI/quotes_ui.dart
+++ lib/features/APIcall/UI/quotes_ui.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import 'package:flutter_bloc/flutter_bloc.dart';
 
+import '../../home/custom_tooltip.dart';
 import '../bloc/quotes_bloc.dart';
 
 class Quotes extends StatelessWidget {
@@ -21,6 +23,9 @@ class QuotesBody extends StatefulWidget {
 }
 
 class _QuotesBodyState extends State<QuotesBody> {
+
+  var copyText = '';
+
   @override
   void initState() {
     context.read<QuotesBloc>().add(GetQuotesInitial());
@@ -29,31 +34,41 @@ class _QuotesBodyState extends State<QuotesBody> {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      shadowColor: Colors.black87,
-      child: BlocBuilder<QuotesBloc, QuotesState>(
-        builder: (context, state) {
-          switch (state.runtimeType) {
-            case QuotesLoadingState:
-              return const Center(
-                child: CircularProgressIndicator(),
-              );
-            case QuotesSuccessfullState:
-              final responseState = state as QuotesSuccessfullState;
-              return Container(
-                margin: const EdgeInsets.only(left: 15, right: 15),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text((responseState.quotes.choices?[0].message?.content)
-                        .toString()),
-                  ],
-                ),
-              );
-            default:
-              return const SizedBox();
-          }
-        },
+    return GestureDetector(
+      onTap: () async =>{
+        await Clipboard.setData(ClipboardData(text: copyText))
+      },
+      child: CustomTooltip(
+        message: 'The Quote is copied!',
+
+
+        child: Card(
+          shadowColor: Colors.black87,
+          child: BlocBuilder<QuotesBloc, QuotesState>(
+            builder: (context, state) {
+              switch (state.runtimeType) {
+                case QuotesLoadingState:
+                  return const Center(
+                    child: CircularProgressIndicator(),
+                  );
+                case QuotesSuccessfullState:
+                  final responseState = state as QuotesSuccessfullState;
+                  copyText =(responseState.quotes.choices?[0].message?.content).toString();
+                  return Container(
+                    margin: const EdgeInsets.only(left: 15, right: 15),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(copyText),
+                      ],
+                    ),
+                  );
+                default:
+                  return const SizedBox();
+              }
+            },
+          ),
+        ),
       ),
     );
   }
---
+++ lib/features/home/custom_tooltip.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+class CustomTooltip extends StatefulWidget {
+  final Widget child;
+  final String message;
+
+  CustomTooltip({required this.child, required this.message});
+
+  @override
+  _CustomTooltipState createState() => _CustomTooltipState();
+}
+
+class _CustomTooltipState extends State<CustomTooltip> {
+  final GlobalKey _key = GlobalKey();
+  OverlayEntry? _overlayEntry;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      key: _key,
+      onTap: _showTooltip,
+      child: widget.child,
+    );
+  }
+
+  void _showTooltip() {
+    final renderBox = _key.currentContext!.findRenderObject() as RenderBox;
+    final targetOffset = renderBox.localToGlobal(Offset.zero);
+    final targetSize = renderBox.size;
+
+    _overlayEntry = OverlayEntry(
+      builder: (context) => Positioned(
+        left: targetOffset.dx + (targetSize.width/4),
+        top: targetOffset.dy - 45, // Adjust this value to position the tooltip
+        child: Material(
+          color: const Color.fromARGB(0, 41, 41, 41),
+          child: CustomPaint(
+            painter: TooltipPainter(),
+            child: Container(
+              padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+              margin: EdgeInsets.only(bottom: 10), // Adjust this for V shape spacing
+              child: Text(
+                widget.message,
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+
+    Overlay.of(context)!.insert(_overlayEntry!);
+
+    Future.delayed(Duration(seconds: 2), () {
+      _overlayEntry?.remove();
+    });
+  }
+}
+
+class TooltipPainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    Paint paint = Paint()
+      ..color = const Color.fromARGB(255, 39, 39, 39)
+      ..style = PaintingStyle.fill;
+
+    Path path = Path()
+      ..addRRect(RRect.fromRectAndRadius(
+        Rect.fromLTWH(0, 0, size.width, size.height - 10), // Adjust height for the V shape
+        Radius.circular(20),
+      ));
+
+    Path vPath = Path()
+      ..moveTo(size.width / 2 - 10, size.height - 10)
+      ..lineTo(size.width / 2, size.height)
+      ..lineTo(size.width / 2 + 10, size.height - 10)
+      ..close();
+
+    canvas.drawPath(path, paint);
+    canvas.drawPath(vPath, paint);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return false;
+  }
+}
