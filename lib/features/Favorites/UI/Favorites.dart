438b2b2fb61bcaddbf702fe7fdc6fd2f4f1a9a49
udaykumar-m <udaykumardevil3830@gmail.com>
2024-07-12 22:23:27 +0530

    Added share icon and updated the list to card style

--- lib/features/Favorites/UI/Favorites.dart
+++ lib/features/Favorites/UI/Favorites.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../bloc/firebase_bloc.dart';
@@ -33,23 +34,7 @@ class _FavoritesState extends State<Favorites> {
               const Center(child: CircularProgressIndicator()),
           FirebaseLoading: () =>
               const Center(child: CircularProgressIndicator()),
-          FirebaseLoaded: (FirebaseLoaded state) => ListView.builder(
-                itemCount: state.data.length,
-                itemBuilder: (context, index) {
-                  return ListTile(
-                    title: Text(state.data[index].word),
-                    subtitle: Text(state.data[index].text),
-                    trailing: IconButton(
-                      onPressed: () {
-                        context
-                            .read<FirebaseBloc>()
-                            .add(DeleteData(state.data[index]));
-                      },
-                      icon: const Icon(Icons.delete),
-                    ),
-                  );
-                },
-              ),
+          FirebaseLoaded: (FirebaseLoaded state) => cardView(state),
           FirebaseError: (FirebaseError state) =>
               Center(child: Text(state.message)),
           FirebaseSuccess: (FirebaseSuccess state) =>
@@ -79,6 +64,60 @@ class _FavoritesState extends State<Favorites> {
           return const Center(child: Text("Something went wrong"));
         }
       }),
+    );
+  }
+
+  ListView cardView(FirebaseLoaded state) {
+    return ListView.builder(
+      itemCount: state.data.length,
+      itemBuilder: (context, index) {
+        return GestureDetector(
+          onTap: () async => await Clipboard.setData(
+              ClipboardData(text: state.data[index].text)),
+          child: Card(
+            margin: const EdgeInsets.only(left: 20.0, right: 20.0, top: 10.0),
+            child: ListTile(
+              title: Padding(
+                padding: const EdgeInsets.only(top: 10.0, bottom: 10.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(state.data[index].word,
+                        style: const TextStyle(fontWeight: FontWeight.bold)),
+                    CircleAvatar(
+                      radius: 24,
+                      backgroundColor:
+                          Theme.of(context).highlightColor.withAlpha(80),
+                      child: CircleAvatar(
+                        radius: 22,
+                        backgroundColor:
+                            Theme.of(context).cardColor.withOpacity(0.8),
+                        child: IconButton(
+                          icon: const Icon(Icons.share),
+                          onPressed: () {},
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              subtitle: Row(
+                children: [
+                  Expanded(child: Text(state.data[index].text)),
+                  IconButton(
+                    onPressed: () {
+                      context
+                          .read<FirebaseBloc>()
+                          .add(DeleteData(state.data[index]));
+                    },
+                    icon: const Icon(Icons.delete),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        );
+      },
     );
   }
 }
